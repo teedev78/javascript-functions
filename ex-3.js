e1de3d0a5cb04e76b6ca92593eb89a08b08fa01b
teedev78 <i7891011t@gmail.com>
2025-02-21 15:07:48 +0700

    feat: Do assignment

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,21 @@
 // Start coding here
+let add = (a, b) => {
+  return a + b;
+};
+
+let subtract = (a, b) => {
+  return a - b;
+};
+
+let multiply = (a, b) => {
+  return a * b;
+};
+
+let divide = (a, b) => {
+  return a / b;
+};
+
+console.log(add(10, 5));
+console.log(subtract(10, 5));
+console.log(multiply(10, 5));
+console.log(divide(10, 5));
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,7 @@
 const minNumber = findMinNumber(10, 20);
 
 console.log(minNumber); // 10
+
+function findMinNumber(a,b) {
+    return Math.min(a,b);
+}
